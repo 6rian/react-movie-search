754ee504618301b56d27f3a2a79245a56397d1f7
Brian Griffin <wbgriffin@gmail.com>
2021-07-13 11:24:33 -0400

    Added static mock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie Search</h1>
+      <form>
+        <input type="search" name="query" />
+        <button type="submit">Search</button>
+      </form>
+      <div className="search-results">
+        <div className="search-results--movie-card">
+          <img
+            src="https://www.themoviedb.org/t/p/w1280/9i3plLl89DHMz7mahksDaAo7HIS.jpg"
+            className="search-results--image"
+            alt="Jurassic Park"
+          />
+          <h2 className="search-results--title">Jurassic Park</h2>
+          <p>Released: 1993-06-11</p>
+          <p>Vote Average: 7.9 / 10</p>
+        </div>
+      </div>
     </div>
   );
 }
