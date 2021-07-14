b9e90dc8a61c2b154f6af9adc90c6e043690e734
Brian Griffin <wbgriffin@pm.me>
2021-07-13 20:14:16 -0400

    Added SearchResults component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import SearchForm from './components/SearchForm';
+import SearchResults from './components/SearchResults';
 import './App.css';
 
 function App() {
@@ -18,18 +19,7 @@ function App() {
     <div className="app">
       <h1>Movie Search</h1>
       <SearchForm query={query} handleChange={handleChange} handleSubmit={handleSubmit} />
-      <div className="search-results">
-        <div className="search-results--movie-card">
-          <img
-            src="https://www.themoviedb.org/t/p/w1280/9i3plLl89DHMz7mahksDaAo7HIS.jpg"
-            className="search-results--image"
-            alt="Jurassic Park"
-          />
-          <h2 className="search-results--title">Jurassic Park</h2>
-          <p>Released: 1993-06-11</p>
-          <p>Vote Average: 7.9 / 10</p>
-        </div>
-      </div>
+      <SearchResults />
     </div>
   );
 }
